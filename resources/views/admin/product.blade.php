baa4138df08cc229f1731032efbe08670551db44
imranhussainlaravel <prinsekhan007@gmail.com>
2024-10-29 16:47:38 +0500

    a

--- resources/views/admin/product.blade.php
+++ resources/views/admin/product.blade.php
@@ -7,6 +7,14 @@
     <link rel="icon" type="image/x-icon" href="{{ URL('images/logo.png') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/admin.css')}}">
+    
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+        <!-- Bootstrap JS (for dismissing alerts) -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+    
 </head>
 <body>
     <div class="app-container">
